04f1dfd5e8f4bbc866f31ed68f6f2c9fa8c0a731
luissj21 <luissanjuan21@outlook.es>
2025-11-22 21:33:21 -0600

    Agregado Sistema de Pausa y Fin de juego

--- Assets/Scripts/PasarNivelScript.cs
+++ Assets/Scripts/PasarNivelScript.cs
@@ -1,25 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PasarNivelScript : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Rambo"))
+        if (!collision.CompareTag("Rambo")) return;
+
+        int current = SceneManager.GetActiveScene().buildIndex;
+
+        // Si estás en el último nivel (Nivel 3)
+        if (current == 3)   // <-- Ajusta al index correcto en tu Build Settings
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene("MenuInicial");
+        }
+        else
+        {
+            SceneManager.LoadScene(current + 1);
         }
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
+
+
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+//public class PasarNivelScript : MonoBehaviour
+//{
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if (collision.CompareTag("Rambo"))
+//        {
+//            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+//        }
+//    }
+
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//}
